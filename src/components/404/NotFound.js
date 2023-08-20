2938e7228e9931e159a5039c0b758581a697257b
Ahmed Anwer <ahmed@Ahmeds-MacBook-Air.local>
2023-08-20 21:12:42 +0530

    Push to main

--- src/components/404/NotFound.js
+++ src/components/404/NotFound.js
@@ -38,7 +38,6 @@ function NotFound() {
                     d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                   />
                 </svg>
-
                 <span>Go back</span>
               </button>
 
