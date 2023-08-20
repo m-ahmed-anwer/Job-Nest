19254e3f8e23c4b68a108589341824901540076b
Ahmed Anwer <ahmed@Ahmeds-MacBook-Air.local>
2023-08-20 21:04:13 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Home from "./routes/home/home.component";
 import Jobs from "./routes/jobs/jobs.component";
+import Login from "./routes/login/login.component";
+import Signup from "./routes/signup/signup.component";
+import ContactUs from "./components/contact-us/contact-us";
+import NotFound from "./components/404/NotFound";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route path="/jobs" element={<Jobs />} />
         <Route path="/about" element={<Jobs />} />
         <Route path="/service" element={<Jobs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
