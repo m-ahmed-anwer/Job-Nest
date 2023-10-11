92fa80e3dc92b55dad73a4326871718da0a83c08
M.A.Ahmed <ahmedanwer0094@gmail.com>
2023-10-11 07:14:01 +0530

    Update Chat Ui

--- src/components/single-company/ChatBox.jsx
+++ src/components/single-company/ChatBox.jsx
@@ -106,6 +106,7 @@ const ChatBox = () => {
           <div
             id="messages"
             className=" flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+            style={{ height: 500 }}
           >
             {Array.isArray(allMessage.send) &&
               Array.isArray(allMessage.receive) &&
